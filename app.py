faa896a5c97def4adad8efea090a8b6b9d24eb61
rajneesh shukla <developer.rajneeshshukla@gmail.com>
2025-08-12 01:04:05 +0530

    final commit

--- app.py
+++ app.py
@@ -270,5 +270,7 @@ def classify_and_describe():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
